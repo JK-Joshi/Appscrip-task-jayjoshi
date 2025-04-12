011962e9b113492bc434fdb90f498f479d6807ce
Jayjoshitjcg <jayjoshi772@gmail.com>
2025-04-12 22:19:04 +0400

    - Update Schema in the project

--- src/app/Pages/Home/Home.tsx
+++ src/app/Pages/Home/Home.tsx
@@ -5,13 +5,22 @@ import Footer from '@/app/Components/Footer/Footer'
 import Header from '@/app/Components/Header/Header'
 import Products from '@/app/Components/Products/Products'
 
+
+//Redux Imports
+import { store } from '@/store';
+
+//Third Party Imports
+import { Provider } from 'react-redux';
+
 const Home = () => {
     return (
         <div>
-            <Header />
-            <Banner />
-            <Products />
-            <Footer />
+            <Provider store={store}>
+                <Header />
+                <Banner />
+                <Products />
+                <Footer />
+            </Provider>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,44 @@
-'use client'
+//Next Imports 
+import Head from 'next/head';
 
 //Custom Components
 import Home from './Pages/Home/Home'
 
-//Redux Imports
-import { store } from '@/store';
-
-//Third Party Imports
-import { Provider } from 'react-redux';
 
 const page = () => {
+
+  const homepageSchema = {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    "name": "FakeStore E-commerce",
+    "description": "Explore top-quality products at the FakeStore built using FakeStoreAPI.",
+    "url": "https://appscrip-task-jayjoshi.netlify.app/",
+    "publisher": {
+      "@type": "Organization",
+      "name": "FakeStore",
+      "logo": {
+        "@type": "ImageObject",
+      }
+    }
+  };
+
+
   return (
     <div>
-      <Provider store={store}>
-        <Home />
-      </Provider>
-    </div>
+      <Head>
+        <title>FakeStore - Home</title>
+        <meta name="description" content="Explore top-quality products at FakeStore built using FakeStoreAPI." />
+        <meta name="robots" content="index, follow" />
+
+        {/* Schema - WebPage */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(homepageSchema) }}
+        />
+      </Head>
+
+      <Home />
+    </div >
   )
 }
 
